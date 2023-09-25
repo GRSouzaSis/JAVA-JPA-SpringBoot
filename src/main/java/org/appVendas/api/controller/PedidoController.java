914175f5efaa2d2079ee0e0e00a0326477ffbd9b
gisouza <givaribeiro23@gmail.com>
2023-09-25 15:36:14 -0300

    Details pedido

--- src/main/java/org/appVendas/api/controller/PedidoController.java
+++ src/main/java/org/appVendas/api/controller/PedidoController.java
@@ -1,9 +1,19 @@
 package org.appVendas.api.controller;
 
+import org.appVendas.api.dto.InformacaoItemPedidoDTO;
+import org.appVendas.api.dto.InformacoesPedidoDTO;
 import org.appVendas.api.dto.PedidoDTO;
+import org.appVendas.domain.entity.ItemPedido;
 import org.appVendas.domain.entity.Pedido;
 import org.appVendas.service.PedidoService;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -21,5 +31,40 @@ public class PedidoController {
     public Integer save(@RequestBody PedidoDTO dto){
         Pedido pedido = service.salvar(dto);
         return pedido.getId();
+    }
+
+    @GetMapping("{id}")
+    public InformacoesPedidoDTO getById(@PathVariable Integer id ){
+        return service
+                .obterPedidoCompleto(id)
+                .map( p -> converter(p) )
+                .orElseThrow(() ->
+                        new ResponseStatusException(NOT_FOUND, "Pedido não encontrado."));
+    }
+
+    private InformacoesPedidoDTO converter(Pedido pedido){
+        return InformacoesPedidoDTO
+                .builder()
+                .codigo(pedido.getId())
+                .dataPedido(pedido.getDataPedido().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
+                .cpf(pedido.getCliente().getCpf())
+                .nomeCliente(pedido.getCliente().getNome())
+                .total(pedido.getTotal())
+//                .status(pedido.getStatus().name())
+                .items(converter(pedido.getItens()))
+                .build();
+    }
+
+    private List<InformacaoItemPedidoDTO> converter(List<ItemPedido> itens){
+        if(CollectionUtils.isEmpty(itens)){
+            return Collections.emptyList();
+        }
+        return itens.stream().map(
+                item -> InformacaoItemPedidoDTO
+                        .builder().descricaoProduto(item.getProduto().getDescricao())
+                        .precoUnitario(item.getProduto().getPreco())
+                        .quantidade(item.getQuantidade())
+                        .build()
+        ).collect(Collectors.toList());
     }
 }
--- src/main/java/org/appVendas/service/PedidoService.java
+++ src/main/java/org/appVendas/service/PedidoService.java
@@ -3,6 +3,9 @@ package org.appVendas.service;
 import org.appVendas.api.dto.PedidoDTO;
 import org.appVendas.domain.entity.Pedido;
 
+import java.util.Optional;
+
 public interface PedidoService {
     Pedido salvar(PedidoDTO dto);
+    Optional<Pedido> obterPedidoCompleto(Integer id);
 }
